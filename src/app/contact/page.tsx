10136d84148626a038163dd3e96171669b3d14ef
Srijan Bista <srijanbista4284@gmail.com>
2024-07-06 18:38:17 +0545

    added containerwrapper

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,9 @@
+import ContainerWrapper from "@/components/Container";
+
 export default function Contact() {
-  return <div className="pt-[120px]">Contact</div>;
+  return (
+    <div className="pt-8 lg:pt-[120px]">
+      <ContainerWrapper>Contact</ContainerWrapper>
+    </div>
+  );
 }
--- src/app/pages/page.tsx
+++ src/app/pages/page.tsx
@@ -1,3 +1,9 @@
+import ContainerWrapper from "@/components/Container";
+
 export default function Pages() {
-  return <div className="pt-[120px]">Pages</div>;
+  return (
+    <div className="pt-8 lg:pt-[120px]">
+      <ContainerWrapper>Pages</ContainerWrapper>
+    </div>
+  );
 }
