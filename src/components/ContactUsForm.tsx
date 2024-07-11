98f00a1f17e4350512e1d709c138dd069b1a2da7
Srijan Bista <srijanbista4284@gmail.com>
2024-07-11 11:49:30 +0545

    added contact us form

---
+++ src/components/ContactUsForm.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { HTMLInputTypeAttribute } from "react";
+import * as yup from "yup";
+
+const ContactUsFormValidationSchema = yup.object().shape({
+  name: yup.string().required("Name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  message: yup.string().required("Message is required").min(20),
+});
+const ContactUsForm = () => {
+  return (
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        message: "",
+      }}
+      validationSchema={ContactUsFormValidationSchema}
+      onSubmit={(values: any) => console.log(values)}
+    >
+      {() => {
+        return (
+          <Form className="border border-slate-400 rounded-md lg:max-w-lg px-4 py-2 lg:px-8 lg:py-4 mx-auto">
+            <h1 className="text-center text-5xl mb-5">Contact Us Form</h1>
+            <FormGroup name="name" placeholder="Enter name" />
+            <FormGroup name="email" placeholder="Enter Email" />
+            <FormGroup name="message" placeholder="Enter Message" />
+            <button
+              type="submit"
+              className="px-4 mt-2 lg:mt-4 py-2 border border-slate-200 rounded-md hover:bg-slate-800 hover:text-white transition-colors duration-200 ease-in-out active:bg-slate-500"
+            >
+              Submit
+            </button>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default ContactUsForm;
+
+export const FormGroup = ({
+  type = "text",
+  name,
+  placeholder,
+  className,
+}: {
+  type?: HTMLInputTypeAttribute;
+  name: string;
+  placeholder: string;
+  className?: HTMLInputTypeAttribute;
+}) => {
+  return (
+    <>
+      <label htmlFor={name} className="capitalize">
+        {name}:
+      </label>
+      <Field
+        name={name}
+        placeholder={placeholder}
+        id={name}
+        type={type}
+        className={`${className} block p-3 w-full mb-1 lg:mb-2 border border-slate-400 rounded-md`}
+      />
+      <ErrorMessage name={name}>
+        {(msg) => <div className="text-red-500">{msg}</div>}
+      </ErrorMessage>
+    </>
+  );
+};
